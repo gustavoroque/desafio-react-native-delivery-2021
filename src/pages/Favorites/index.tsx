42cd94e477630fb2022cf242cdd5f0dc5a5a4afe
roque <gustavohroque@hotmail.com>
2021-06-15 14:21:55 -0300

    Finalizado Desafio.

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,11 +32,18 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const response = await api.get('/favorites');
+
+      setFavorites(
+        response.data.map((favorite: Food) => ({
+          ...favorite,
+          formattedPrice: formatValue(favorite.price),
+        })),
+      );
     }
 
     loadFavorites();
-  }, []);
+  }, [setFavorites]);
 
   return (
     <Container>
